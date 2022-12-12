8e20cc09358fa43f5c14d3f367e80a900bc53d7c
dnfwlxo11 <dnfwlxo11@naver.com>
2022-12-12 14:59:47 +0900

    add: unit test code example

--- nest-test/src/movies/movies.controller.ts
+++ nest-test/src/movies/movies.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
-import { urlToHttpOptions } from 'url';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -13,27 +14,27 @@ export class MoviesController {
   }
 
   @Get('search')
-  search(@Query('year') searchYear: string) {
+  search(@Query('year') searchYear: number) {
     return `Search movie with year ${searchYear}`;
   }
 
   @Get(':id')
-  getOne(@Param('id') movieId: string): Movie {
+  getOne(@Param('id') movieId: number): Movie {
     return this.moviesService.getOne(movieId);
   }
 
   @Post()
-  create(@Body() movieData) {
+  create(@Body() movieData: CreateMovieDto) {
     return this.moviesService.create(movieData);
   }
 
   @Delete(':id')
-  deleteMovie(@Param('id') movieId: string) {
+  deleteMovie(@Param('id') movieId: number) {
     return this.moviesService.deleteOne(movieId);
   }
 
   @Patch(':id')
-  patchMovie(@Param('id') movieId: string, @Body() updateData) {
+  patchMovie(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
     return this.moviesService.update(movieId, updateData);
   }
 }
--- nest-test/src/movies/movies.service.ts
+++ nest-test/src/movies/movies.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -9,8 +10,8 @@ export class MoviesService {
     return this.movies;
   }
 
-  getOne(id: string): Movie {
-    const movie = this.movies.find(movie => movie.id === +id);
+  getOne(id: number): Movie {
+    const movie = this.movies.find(movie => movie.id === id);
 
     if (!movie) {
       throw new NotFoundException(`Movie with ID ${id} not found.`);
@@ -19,9 +20,9 @@ export class MoviesService {
     return movie; 
   }
 
-  deleteOne(id: string) {
+  deleteOne(id: number) {
     this.getOne(id);
-    this.movies = this.movies.filter(movie => movie.id !== +id);
+    this.movies = this.movies.filter(movie => movie.id !== id);
   }
 
   create(movieData) {
@@ -31,7 +32,7 @@ export class MoviesService {
     })
   }
 
-  update(id: string, updateData) {
+  update(id: number, updateData: UpdateMovieDto) {
     const movie = this.getOne(id);
 
     this.deleteOne(id);
